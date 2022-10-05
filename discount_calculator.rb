42d429e56c4fbed0ec7d4da6ac96ee824cd74b66
Mike Bowman <mbowman2@gmail.com>
2022-10-04 21:29:13 -0400

    Add basic skeleton and order normalization

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -1,5 +1,24 @@
 class DiscountCalculator
-  def sanity_check
-    "It works!"
+  attr_reader :order
+
+  SINGLE_SHIRT_PRICE = 800 # Price in cents
+  ORDER_DEFAULTS = {
+    shirt_1: 0,
+    shirt_2: 0,
+    shirt_3: 0,
+    shirt_4: 0,
+    shirt_5: 0
+  }.freeze
+
+  def initialize(order = {})
+    @order = normalize_order(order)
+  end
+
+  def calculate
+    :not_implemented
+  end
+
+  def normalize_order(order)
+    ORDER_DEFAULTS.merge(order).slice(*ORDER_DEFAULTS.keys)
   end
 end
--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -1,9 +1,31 @@
 require_relative "../discount_calculator"
 
 RSpec.describe DiscountCalculator do
-  context "#sanity_check" do
-    it "works!" do
-      expect(described_class.new.sanity_check).to eq "It works!"
+  context "#calculate" do
+    it "correctly calculates the discount for a complex order" do
+      skip
+      order = {shirt_1: 2, shirt_2: 2, shirt_3: 2, shirt_4: 1, shirt_5: 1}
+      calculator = DiscountCalculator.new(order)
+
+      expected_price_cents = 5120
+
+      expect(calculator.calculate).to eq(expected_price_cents)
+    end
+  end
+
+  context "#normalize_order" do
+    it "ensures valid default values are set for all new orders" do
+      input = {shirt_1: 3}
+      result = DiscountCalculator.new.normalize_order(input)
+
+      expect(result).to eq({shirt_1: 3, shirt_2: 0, shirt_3: 0, shirt_4: 0, shirt_5: 0})
+    end
+
+    it "removed invalid keys" do
+      input = {shirt_1: 2, foo: 15, bar: :invalid}
+      result = DiscountCalculator.new.normalize_order(input)
+
+      expect(result).to eq({shirt_1: 2, shirt_2: 0, shirt_3: 0, shirt_4: 0, shirt_5: 0})
     end
   end
 end
