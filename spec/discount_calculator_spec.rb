06ca01b06210bf4c5aa6ff358e434cd802665b99
Mike Bowman <mbowman2@gmail.com>
2022-10-06 16:36:41 -0400

    Add tests for cases where no discounts are possible

--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -4,9 +4,23 @@ RSpec.describe DiscountCalculator do
   context "#calculate" do
     it "applies only the 5-piece discount if that's all that's possible" do
       order = {shirt_1: 1, shirt_2: 1, shirt_3: 1, shirt_4: 1, shirt_5: 1}
-      expected_price = 3000
+      expected_price_cents = 3000
 
-      expect(DiscountCalculator.new(order).calculate).to eq expected_price
+      expect(DiscountCalculator.new(order).calculate).to eq expected_price_cents
+    end
+
+    it "applies no discount if the order contains a single item" do
+      order = {shirt_1: 1}
+      expected_price_cents = 800
+
+      expect(DiscountCalculator.new(order).calculate).to eq expected_price_cents
+    end
+
+    it "applies no discount if the order contains multiple copies of the same item" do
+      order = {shirt_1: 2}
+      expected_price_cents = 1600
+
+      expect(DiscountCalculator.new(order).calculate).to eq expected_price_cents
     end
 
     it "correctly calculates the discount for a complex order" do
