ee7a1f6b565e992ec56e8ba1e40d08789f444200
Mike Bowman <mbowman2@gmail.com>
2022-10-05 20:44:07 -0400

    Simple logic to figure out which discounts can be applied

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -1,6 +1,6 @@
 class DiscountCalculator
   attr_reader :order
-  attr_accessor :discount
+  attr_accessor :applied_discounts
 
   SINGLE_SHIRT_PRICE = 800 # Price in cents
   ORDER_DEFAULTS = {
@@ -10,22 +10,28 @@ class DiscountCalculator
     shirt_4: 0,
     shirt_5: 0
   }.freeze
+  DISCOUNTS = [
+    {id: 1, items: 2, amount: 5},
+    {id: 2, items: 3, amount: 10},
+    {id: 3, items: 4, amount: 20},
+    {id: 4, items: 5, amount: 25}
+  ].freeze
 
   def initialize(order = {})
     @order = normalize_order(order)
     @discount = []
+    @applied_discounts = []
   end
 
   def calculate
-    puts "HERE"
-    5.downto(2).each_with_object(order.dup) do |num, order|
-      require "debug"
-      binding.break
-      if order.all? { |_product, quantity| quantity >= 1 } # discount available
-        discount << [5, 25]
-        order.each { |product, quantity| order[product] = quantity - 1 unless quantity.zero? }
-      end
+  def applicable_discounts
+    DISCOUNTS.each do |discount|
+      applied_discounts << discount if discount_applies?(discount)
     end
+  end
+
+  def discount_applies?(discount)
+    order.count { |_item, quantity| quantity > 0 } >= discount[:items]
   end
 
   def normalize_order(order)
--- spec/discount_calculator_spec.rb
+++ spec/discount_calculator_spec.rb
@@ -12,6 +12,22 @@ RSpec.describe DiscountCalculator do
     end
   end
 
+  context "#discount_applies?" do
+    it "returns true if the discount can be applied to the order" do
+      order = {shirt_1: 1, shirt_2: 2}
+      discount = {id: 1, items: 2, amount: 5}
+
+      expect(DiscountCalculator.new(order).discount_applies?(discount)).to be true
+    end
+
+    it "returns false if the discount cannot be applied to the order" do
+      order = {shirt_1: 1, shirt_2: 2, shirt_3: 0}
+      discount = {id: 1, items: 3, amount: 5}
+
+      expect(DiscountCalculator.new(order).discount_applies?(discount)).to be false
+    end
+  end
+
   context "#normalize_order" do
     it "ensures valid default values are set for all new orders" do
       input = {shirt_1: 3}
