ac3281f9ff32e397339ea74fc13c08566de728c9
Mike Bowman <mbowman2@gmail.com>
2022-10-06 16:29:41 -0400

    Document some of the private methods

--- discount_calculator.rb
+++ discount_calculator.rb
@@ -15,6 +15,8 @@ class DiscountCalculator
     @order = normalize_order(raw_order)
   end
 
+  # Returns the final price of the order, in cents, *after* the greatest possible
+  # discount has been applied
   def calculate
     base_order_price = order.count * SINGLE_SHIRT_PRICE
     total_discount = generate_possible_discount_combinations
@@ -27,20 +29,28 @@ class DiscountCalculator
   # TODO: Should these methods all be private? Technically, they're implementation details
   private
 
+  # Returns the discount record corresponding to the number of items in the group.
+  # Returns `nil` if no discount can be applied for the given size
   def discount_for_group_size(size)
-    return {price_savings: 0} if size == 1
-
     DISCOUNTS.find { |discount| discount[:items] == size }
   end
 
+  # Returns either the `:price_savings` field from the relevant discount, or 0
+  # if no discount can be applied to the provided size
   def price_savings_for_group_size(size)
-    discount_for_group_size(size)[:price_savings]
+    discount = discount_for_group_size(size)
+
+    return 0 unless discount
+
+    discount[:price_savings]
   end
 
   def total_savings_for_discount_combination(group)
     group.map(&:count).map { |c| price_savings_for_group_size(c) }.sum
   end
 
+  # TODO: may be possible to replace these two following methods with a single call to reduce
+  # Enumerates all applicable discount combinations for an order.
   def generate_possible_discount_combinations
     discount_items_range.to_a.reverse_each.with_object([]) do |n, combinations|
       groups = []
